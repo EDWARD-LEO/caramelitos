61dafd0688097708922977552a3b3079537c9028
JhonFM <jfranciaminaya@gmail.com>
2023-03-02 18:58:28 -0500

    Ejemplo 2: Datatable y actualización de datos de ejemplo

--- data/proveedores.php
+++ data/proveedores.php
@@ -2,10 +2,20 @@
 
 //Data de prueba (el primer valor representaría al ID - PK)
 $listProviders = [
-  [17, "Corporación Tecnológica A", "Ecuador", "corporacion_a@gmail.com", "956111111"],
-  [20, "Corporación Tecnológica B", "Chile", "corporacion_b@gmail.com", "956111112"],
-  [23, "Corporación Tecnológica C", "Brasil", "corporacion_c@gmail.com", "956111113"],
-  [31, "Corporación Tecnológica D", "Bolivia", "corporacion_d@gmail.com", "956111114"]
+  [1, "Corporación Tecnológica A", "Ecuador", "corporacion_a@gmail.com", "956111111"],
+  [2, "Corporación Tecnológica B", "Chile", "corporacion_b@gmail.com", "956111112"],
+  [3, "Corporación Tecnológica C", "Brasil", "corporacion_c@gmail.com", "956111113"],
+  [5, "Corporación Tecnológica D", "Bolivia", "corporacion_d@gmail.com", "956111114"],
+  [7, "Corporación Tecnológica E", "Ecuador", "corporacion_a@gmail.com", "956111111"],
+  [8, "Corporación Tecnológica F", "Chile", "corporacion_b@gmail.com", "956111112"],
+  [9, "Corporación Tecnológica G", "Brasil", "corporacion_c@gmail.com", "956111113"],
+  [10, "Corporación Tecnológica H", "Bolivia", "corporacion_d@gmail.com", "956111114"],
+  [15, "Corporación Tecnológica I", "Ecuador", "corporacion_a@gmail.com", "956111111"],
+  [16, "Corporación Tecnológica J", "Chile", "corporacion_b@gmail.com", "956111112"],
+  [17, "Corporación Tecnológica K", "Brasil", "corporacion_c@gmail.com", "956111113"],
+  [18, "Corporación Tecnológica L", "Bolivia", "corporacion_d@gmail.com", "956111114"],
+  [19, "Corporación Tecnológica M", "Ecuador", "corporacion_a@gmail.com", "956111111"],
+  [20, "Corporación Tecnológica N", "Chile", "corporacion_b@gmail.com", "956111112"]
 ];
 
 if (isset($_POST['operacion'])){
